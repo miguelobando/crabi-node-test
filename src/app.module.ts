baaf800a948cde14170d7ffeb0a0e1ea7cecf21b
Miguel Obando <miguel.obando204@gmail.com>
2025-02-14 10:52:01 -0500

    🔐  Add database credentials to .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      password: 'postgrespassword',
-      username: 'postgres',
-      database: 'database',
-      schema: 'public',
-      synchronize: true,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME,
+      database: process.env.DB_NAME,
+      schema: process.env.DB_SCHEMA,
+      synchronize: process.env.DB_SYNC === 'true',
       autoLoadEntities: true,
-      poolSize: 10,
+      poolSize: parseInt(process.env.DB_POOL_SIZE),
     }),
   ],
   controllers: [AppController],
