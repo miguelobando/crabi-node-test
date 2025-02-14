513d04aae0ba5576e33424eeed3c4cff0b227261
Miguel Obando <miguel.obando204@gmail.com>
2025-02-14 09:40:41 -0500

    🧱  Add infra folder and db

---
+++ infra/init/seed.sql
@@ -0,0 +1,10 @@
+-- Create tables
+CREATE TABLE IF NOT EXISTS public.users (
+    id UUID NOT NULL DEFAULT gen_random_uuid(),
+    first_name VARCHAR(100) not null,
+    last_name VARCHAR(100) not null,
+    password varchar(255) not null,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    black_listed boolean not null
+);
